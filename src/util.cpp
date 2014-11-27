8aef422c03adae9f901f974b638ce52c17c0264d
SigmundAlpha <alpha.sigmund@gmail.com>
2014-11-26 21:10:13 -0500

    Directory name changes in util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -1059,7 +1059,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.ppcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "PPCoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Peercoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -1071,10 +1071,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     fs::create_directory(pathRet);
-    return pathRet / "PPCoin";
+    return pathRet / "Peercoin";
 #else
     // Unix
-    return pathRet / ".ppcoin";
+    return pathRet / ".peercoin";
 #endif
 #endif
 }
@@ -1115,7 +1115,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "ppcoin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "peercoin.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
@@ -1149,7 +1149,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "ppcoind.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "peercoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
@@ -1382,7 +1382,7 @@ void AddTimeData(const CNetAddr& ip, int64 nTime)
                 if (!fMatch)
                 {
                     fDone = true;
-                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong PPCoin will not work properly.");
+                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong Peercoin will not work properly.");
                     strMiscWarning = strMessage;
                     printf("*** %s\n", strMessage.c_str());
                     uiInterface.ThreadSafeMessageBox(strMessage, "", CClientUIInterface::MSG_WARNING);
